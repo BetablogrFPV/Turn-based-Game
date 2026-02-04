2cfa458235e8ff1bd38f64a8eae53e32c45f043d
J. <jschoerner@gmx.de>
2026-02-04 18:58:07 +0100

    pygame tructure

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-print("hello Wolrd") 
-return
+import pygame
+import sys
+
+WIDTH, HEIGHT = 800, 600
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Mein erstes Pygame-Fenster")
+clock = pygame.time.Clock()
+
+#gameloop
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((30, 200, 30))
+
+ 
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
